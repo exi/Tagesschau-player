4a8a0d896de890580e719b7db36b65306bcf072d
reno reckling <reno.reckling@student.kit.edu>
2010-10-20 07:57:51 +0200

    added live and recent shows

--- play.pl
+++ play.pl
@@ -2,14 +2,11 @@
 
 use LWP::Simple;
 use HTML::TreeBuilder::XPath;
+use Switch;
 
 sub pageRequest {
-    if(@_ < 1) {
-        print "not enough parameter in pageRequest\n";
-        return;
-    }
+    my ($url) = @_;
 
-    my $url = @_[0];
     my $content = get $url;
     die "Can't fetch url $url\n" unless defined $content;
     return $content;
@@ -24,22 +21,49 @@ sub findXPath {
     return @nodeset;
 }
 
-sub findFlashPage {
+sub extractOggStream {
+    my ($content) = @_;
+    my @playercontrol = findXPath($content,'//div[@id="playercontrol"]/div[@class="block"]/a[position()=2');
+    my $streampath = @playercontrol[0]->attr('href');
+    return $streampath;
+}
+
+sub getMainPageContent {
     my $tagesschaumain = 'http://www.tagesschau.de';
     my $content = pageRequest($tagesschaumain);
+    return $content;
+}
+
+sub findLast20Url {
+    my $content = getMainPageContent();
     my @nodes = findXPath($content,'//select[@id="navSendungen"]');
     my $url = @nodes[0]->content()->[0]->attr('value');
     return $url;
 
 }
 
-sub findStreamUrl {
-    my $flashpageurl = findFlashPage();
-    print "Current URL:$flashpageurl \n";
+sub findLast20Stream {
+    my $flashpageurl = findLast20Url();
     my $flashpage = pageRequest($flashpageurl);
-    my @playercontrol = findXPath($flashpage,'//div[@id="playercontrol"]/div[@class="block"]/a[position()=2');
-    my $streampath = @playercontrol[0]->attr('href');
-    print "Stream Path: $streampath\n";
+    my $streampath = extractOggStream($flashpage);
+    return $streampath;
+}
+
+sub findLiveStream {
+    return "http://www.tagesschau.de/commons/include/multimedia/style_video_wmv_live_cover.jsp?res=high&typ=1"
+}
+
+sub findRecentUrl {
+    my $content = getMainPageContent();
+    my @lastshownode = findXPath($content,'//div[@id="sendungenLeft"]/ul/li[position()=3]/a');
+    my $lastshowurl = @lastshownode[0]->attr('href');
+    return $lastshowurl;
+}
+
+sub findRecentStream {
+    my $recentpageurl = findRecentUrl();
+    my $recentpage = pageRequest($recentpageurl);
+    my $streampath = extractOggStream($recentpage);
     return $streampath;
 }
 
@@ -48,4 +72,21 @@ sub mplayerPlay {
     exec("mplayer '$url'");
 }
 
-mplayerPlay(findStreamUrl());
+my ($toplay) = @ARGV;
+my $url;
+
+print "toplay:$toplay\n";
+if (!$toplay) {
+    $url = findLast20Stream();
+}else {
+    switch($toplay) {
+        case "live" {
+            $url = findLiveStream();
+        }
+        case "recent" {
+            $url = findRecentStream();
+        }
+    }
+}
+
+mplayerPlay($url);
